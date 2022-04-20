a82c200693e3cc73725f1efacea50d997952b619
Murray Hat <77835100+Prodevking1@users.noreply.github.com>
2022-04-20 21:41:51 +0000

    initial

---
+++ lib/ressources/themes.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/* Colors */
+
+Color darkOrange = const Color.fromRGBO(183, 65, 14, 1);
+Color blackText = Colors.black;
+Color whiteText = Colors.white;
+Color greyText = const Color.fromRGBO(82, 82, 82, 1);
+
+/* Fonts */
+TextStyle heading1 = const TextStyle(
+  fontSize: 24.0,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Lato',
+  color: Colors.white,
+);
+
+TextStyle heading2 = const TextStyle(
+  fontSize: 24.0,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Lato',
+  color: Colors.black,
+);
+
+TextStyle heading3 = const TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Lato',
+  color: Colors.black,
+);
+
+TextStyle heading4 = const TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Lato',
+  color: Colors.white,
+);
